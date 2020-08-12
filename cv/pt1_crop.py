450a63279edac787145b6b2c65e4b55ef7e14b81
sh <shsong930@gmail.com>
2020-08-13 03:45:40 +0900

    upload

---
+++ cv/pt1_crop.py
@@ -0,0 +1,16 @@
+# 사진에 동그라미 그리고 1/4로 잘라서 검게 채우기
+
+import cv2
+sample = cv2.imread('/Users/uchu/Works/cv/dang.png')
+height, width, rgb = sample.shape
+print(height, width, rgb)
+#544 776 3
+cv2.circle(sample, (width//2, height//2), 100, (0, 0, 255), 10)
+
+crop_sample = sample[:height//2,:width//2,:]
+
+crop_sample[:,:,:] = 0
+
+cv2.imshow('original', sample)
+cv2.imshow('croped', crop_sample)
+cv2.waitKey(0)
